743463dce56003931bfe39b3fef61df4813d30c1
bhanusrikari <bhanusrireddy1511@gmail.com>
2026-03-10 11:54:18 +0530

    Fixed CORS to allow production frontend requests

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: process.env.FRONTEND_URL || '*', // Allow Netlify URL or all in production
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
